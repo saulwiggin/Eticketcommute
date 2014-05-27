093d3ea7f9b27e7e5c86709811e3fed46e252aa6
saul wiggin <saulwiggin@googlemail.com>
2014-05-27 21:42:38 +0100

    lkuijhkljh
    lkj;lkj

---
+++ HelloAndroid.java
@@ -0,0 +1,11 @@
+package com.example.helloandroid;
+import android.app.Activity; 
+import android.os.Bundle;
+public class HelloAndroid extends Activity { 
+/** Called when the activity is first created. */ 
+@Override 
+public void onCreate(BundlesavedInstanceState) { 
+super.onCreate(savedInstanceState);
+setContentView(R.layout.main);
+}
+}
